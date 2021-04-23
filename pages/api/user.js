3c721fecabd582183858f4219f13d4f6de615838
Leonardo Roese <leo.roese@hotmail.com>
2021-04-23 14:00:16 -0600

    part10

--- pages/api/user.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-    if (req.method === 'POST') {
-      res.status(200).json({ name: req.body.user})
-    } else {
-        res.status(200).json({ name: 'Get Request'})
-    }
-  }
