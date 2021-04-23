271897cd92c325384e9c4306623c543a239bd0e3
Leonardo Roese <leo.roese@hotmail.com>
2021-04-23 10:49:14 -0600

    Part 2

---
+++ src/components/Button.js
@@ -0,0 +1,7 @@
+import styles from '../../styles/Button.module.css'
+
+const Button = () => {
+    return <button className={styles.prettierButton} type="button">I am super simple bro</button>
+}
+
+export default Button;
